cc2c8777f00daf0939e2c6de6a5922440dde588c
Wanderson <wandersonduarte.br@gmail.com>
2023-01-19 06:04:04 -0300

    Implementação do FindAll para Product

--- APIs/internal/infra/database/product_db.go
+++ APIs/internal/infra/database/product_db.go
@@ -41,3 +41,20 @@ func (p *Product) DeleteProduct(id string) error {
 	}
 	return p.DB.Delete(product).Error
 }
+
+func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+	var products []entity.Product
+	var err error
+
+	if sort != "" && sort != "asc" && sort != "desc" {
+		sort = "asc"
+	}
+
+	if page != 0 && limit != 0 {
+		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at " + sort).Find(&products).Error
+	} else {
+		err = p.DB.Order("created_at " + sort).Find(&products).Error
+	}
+
+	return products, err
+}
