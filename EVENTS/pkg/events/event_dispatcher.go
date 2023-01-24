3e7df6849c2f44ccfaade70bbec991fbc39f6579
Wanderson <wandersonduarte.br@gmail.com>
2023-01-24 18:08:14 -0300

    Implementando o Register

---
+++ EVENTS/pkg/events/event_dispatcher.go
@@ -0,0 +1,31 @@
+package events
+
+import (
+	"errors"
+)
+
+var ErrHandlerAlreadyRegistered = errors.New("handler already resgistered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
