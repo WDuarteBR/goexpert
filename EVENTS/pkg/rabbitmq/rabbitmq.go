fdfbdaa290ac1f6b6fa89da177f59a2ae9db5534
WDuarteBR <wduarte.br@gmail.com>
2023-02-05 18:10:03 -0300

    Implementado rabbitmq

---
+++ EVENTS/pkg/rabbitmq/rabbitmq.go
@@ -0,0 +1,17 @@
+package rabbitmq
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+func OpenChannel() (*amqp.Channel, error) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	return ch, nil
+}
